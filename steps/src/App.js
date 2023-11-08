cea2840786186c3535ac6c55a9c4a51c24fb58d7
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-11-08 07:58:25 +0300

    UPDATE steps

--- steps/src/App.js
+++ steps/src/App.js
@@ -8,51 +8,65 @@ const messages = [
 
 const App = () => {
 	const [step, setStep] = useState(0);
+	const [isOpen, setIsOpen] = useState(true);
+
+	const toggleStepsHandler = () => {
+		setIsOpen((prevState) => !prevState);
+	};
 
 	const prevHandler = () => {
 		if (step > 0) {
-			setStep((prevState) => --prevState);
+			setStep((prevState) => prevState - 1);
 		}
 	};
 
 	const nextHandler = () => {
 		if (step < messages.length) {
-			setStep((prevState) => ++prevState);
+			setStep((prevState) => prevState + 1);
 		}
 	};
 
 	return (
-		<div className="steps">
-			<div className="numbers">
-				{messages.map((msg, index) => {
-					return (
-						<div
-							key={index}
-							className={step >= index ? 'active' : ''}
+		<main>
+			<button className="close" onClick={toggleStepsHandler}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						{messages.map((msg, index) => {
+							return (
+								<div
+									key={index}
+									className={step >= index ? 'active' : ''}
+								>
+									{index + 1}
+								</div>
+							);
+						})}
+					</div>
+					<p className="message">{messages[step]}</p>
+					<div className="buttons">
+						<button
+							className={step > 0 ? 'active' : ''}
+							disabled={step === 0}
+							onClick={prevHandler}
 						>
-							{index}
-						</div>
-					);
-				})}
-			</div>
-			<p className="message">{messages[step]}</p>
-			<div className="buttons">
-				<button
-					className={step > 0 ? 'active' : ''}
-					disabled={step === 0}
-					onClick={prevHandler}
-				>
-					Previous
-				</button>
-				<button
-					className={step < messages.length - 1 ? 'active' : ''}
-					disabled={step >= messages.length - 1}
-					onClick={nextHandler}
-				>
-					Next
-				</button>
-			</div>
-		</div>
+							Previous
+						</button>
+						<button
+							className={
+								step < messages.length - 1 ? 'active' : ''
+							}
+							disabled={step >= messages.length - 1}
+							onClick={nextHandler}
+						>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</main>
 	);
 };
 
