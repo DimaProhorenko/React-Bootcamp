c1977784122d91faf32faf1120dfe2dcfba00596
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-11-08 21:35:42 +0300

    FINISH eat-n-split

--- eat-n-split/src/components/App.js
+++ eat-n-split/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import SplitBillForm from './SplitBillForm';
 import Storage from '../Storage';
@@ -6,6 +6,14 @@ import Storage from '../Storage';
 function App() {
 	const [friends, setFriends] = useState(Storage.getArrayItem('friends'));
 	const [selectedFriend, setSelectedFriend] = useState({});
+
+	// useEffect(() => {
+
+	// })
+
+	useEffect(() => {
+		Storage.setArrayItem('friends', friends);
+	}, [friends]);
 
 	const addFriendHandler = (friendObj) => {
 		setFriends((prevState) => {
@@ -26,6 +34,17 @@ function App() {
 		}
 	};
 
+	const splitBillHandler = (amount) => {
+		console.log(amount);
+		setFriends((prevState) =>
+			prevState.map((el) =>
+				el.id === selectedFriend.id
+					? { ...el, balance: el.balance + amount }
+					: el
+			)
+		);
+	};
+
 	return (
 		<main className="app">
 			<Sidebar
@@ -35,7 +54,10 @@ function App() {
 				onSelectFriend={selectFriendHandler}
 			/>
 			{Object.keys(selectedFriend).length > 0 && (
-				<SplitBillForm friend={selectedFriend} />
+				<SplitBillForm
+					friend={selectedFriend}
+					onSplit={splitBillHandler}
+				/>
 			)}
 		</main>
 	);
--- eat-n-split/src/components/SplitBillForm.js
+++ eat-n-split/src/components/SplitBillForm.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './Button';
 
-function SplitBillForm({ friend }) {
+function SplitBillForm({ friend, onSplit }) {
+	const [bill, setBill] = useState('');
+	const [paidByUser, setPaidByUser] = useState('');
+	const [whoPays, setWhoPays] = useState('user');
+	const paidByFriend = bill ? bill - paidByUser : '';
+
+	const billChangeHandler = (e) => {
+		setBill(e.target.value);
+	};
+	const paidByUserChangeHandler = (e) => {
+		setPaidByUser(e.target.value);
+	};
+
+	const whoPaysChangeHandler = (e) => {
+		setWhoPays(e.target.value);
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		onSplit(whoPays === 'user' ? paidByFriend : paidByUser * -1);
+		setBill('');
+		setPaidByUser('');
+		setWhoPays('user');
+	};
+
 	return (
-		<form className="form-split-bill">
+		<form className="form-split-bill" onSubmit={submitHandler}>
 			<h2>Split the bill with {friend.name}</h2>
 			<label htmlFor="split-bill-value">Bill value</label>
-			<input id="split-bill-value" type="text" />
+			<input
+				id="split-bill-value"
+				type="text"
+				value={bill}
+				onChange={billChangeHandler}
+			/>
 
 			<label htmlFor="split-your-expance">Your expance</label>
-			<input id="split-your-expance" type="text" />
+			<input
+				id="split-your-expance"
+				type="text"
+				value={paidByUser}
+				onChange={paidByUserChangeHandler}
+			/>
 
 			<label htmlFor="split-friend-expance">
 				{friend.name}'s expance
 			</label>
-			<input id="split-friend-expance" type="text" />
+			<input
+				id="split-friend-expance"
+				type="text"
+				value={paidByFriend}
+				readOnly
+				disabled
+			/>
 
 			<label htmlFor="split-who-pays">Who's paying?</label>
-			<select id="split-who-pays">
+			<select
+				id="split-who-pays"
+				value={whoPays}
+				onChange={whoPaysChangeHandler}
+			>
 				<option value="user">You</option>
 				<option value="friend">{friend.name}</option>
 			</select>
+			<Button>Split Bill</Button>
 		</form>
 	);
 }
