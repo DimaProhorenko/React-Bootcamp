7c882abbf000a472e66c9bfba8cb6c17030a0ec9
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-11-09 15:15:06 +0300

    ADD StarRating component

--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import './style.css';
+import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<StarRating
+			size={48}
+			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+			className="font"
+		/>
 	</React.StrictMode>
 );
