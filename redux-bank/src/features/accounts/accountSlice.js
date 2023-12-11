db77e75daf01f16baf7b0929c0c41c8cdf77ad60
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-12-11 22:18:54 +0300

    UPDATE redux slice to use redux-toolkit

--- redux-bank/src/features/accounts/accountSlice.js
+++ redux-bank/src/features/accounts/accountSlice.js
@@ -13,27 +13,53 @@ const accountSlice = createSlice({
 	reducers: {
 		deposit(state, action) {
 			state.balance += action.payload;
+			state.isLoading = false;
 		},
 		withdraw(state, action) {
 			state.balance -= action.payload;
 		},
-		requestLoan(state, action) {
-			console.log(action);
-			if (state.loan > 0) return;
+		requestLoan: {
+			prepare(amount, purpose) {
+				return {
+					payload: { amount, purpose },
+				};
+			},
+			reducer(state, action) {
+				console.log(action);
+				if (state.amount > 0) return;
 
-			state.loan = action.payload.loan;
-			state.loanPurpose = action.payload.loanPurpose;
-			state.balance += action.payload.loan;
+				state.loan = action.payload.amount;
+				state.loanPurpose = action.payload.purpose;
+				state.balance += action.payload.amount;
+			},
 		},
 		payLoan(state) {
 			state.balance -= state.loan;
 			state.loanPurpose = '';
 			state.loan = 0;
 		},
+		convertCurrency(state) {
+			state.isLoading = true;
+		},
 	},
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+	if (currency === 'USD') {
+		return { type: 'account/deposit', payload: amount };
+	}
+	return async (dispatch) => {
+		dispatch({ type: 'account/convertCurrency' });
+		const res = await fetch(
+			`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+		);
+		const data = await res.json();
+		const converted = data.rates.USD;
+		dispatch({ type: 'account/deposit', payload: converted });
+	};
+}
 
 export default accountSlice.reducer;
 
--- redux-bank/src/features/customers/customersSlice.js
+++ redux-bank/src/features/customers/customersSlice.js
@@ -1,40 +1,71 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
 	fullName: '',
 	nationalId: '',
 	createdAt: '',
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-	switch (action.type) {
-		case 'customer/createCustomer':
-			return {
-				...state,
-				fullName: action.payload.fullName,
-				nationalId: action.payload.nationalId,
-				createdAt: action.payload.createdAt,
-			};
-		case 'customer/updateFullName':
-			return { ...state, fullName: action.payload };
-
-		default:
-			return { ...state };
-	}
-}
-
-export function createCustomer(fullName, nationalId) {
-	return {
-		type: 'customer/createCustomer',
-		payload: {
-			fullName,
-			nationalId,
-			createdAt: new Date().toISOString(),
+const customerSlice = createSlice({
+	name: 'customer',
+	initialState,
+	reducers: {
+		createCustomer: {
+			prepare(fullName, nationalId) {
+				return {
+					payload: {
+						fullName,
+						nationalId,
+					},
+				};
+			},
+			reducer(state, action) {
+				state.fullName = action.payload.fullName;
+				state.nationalId = action.payload.nationalId;
+				state.createdAt = new Date().toISOString();
+			},
 		},
-	};
-}
+		updateFullName(state, action) {
+			state.fullName = action.payload;
+		},
+	},
+});
 
-export function updateFullName(fullName) {
-	return {
-		type: 'customer/updateName',
-		payload: fullName,
-	};
-}
+export const { createCustomer, updateFullName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+// 	switch (action.type) {
+// 		case 'customer/createCustomer':
+// 			return {
+// 				...state,
+// 				fullName: action.payload.fullName,
+// 				nationalId: action.payload.nationalId,
+// 				createdAt: action.payload.createdAt,
+// 			};
+// 		case 'customer/updateFullName':
+// 			return { ...state, fullName: action.payload };
+
+// 		default:
+// 			return { ...state };
+// 	}
+// }
+
+// export function createCustomer(fullName, nationalId) {
+// 	return {
+// 		type: 'customer/createCustomer',
+// 		payload: {
+// 			fullName,
+// 			nationalId,
+// 			createdAt: new Date().toISOString(),
+// 		},
+// 	};
+// }
+
+// export function updateFullName(fullName) {
+// 	return {
+// 		type: 'customer/updateName',
+// 		payload: fullName,
+// 	};
+// }
