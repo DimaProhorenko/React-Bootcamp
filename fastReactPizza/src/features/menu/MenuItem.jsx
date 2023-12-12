c94b7f1072b6f2c4f0c12545611911152877e41b
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-12-12 11:48:32 +0300

    ADD react-router menu data fetching

--- fastReactPizza/src/features/menu/MenuItem.jsx
+++ fastReactPizza/src/features/menu/MenuItem.jsx
@@ -1,18 +1,36 @@
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+import PropTypes from 'prop-types';
 
-  return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
-      </div>
-    </li>
-  );
+import { formatCurrency } from '../../utils/helpers';
+
+function MenuItem({ pizza }) {
+	const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+	return (
+		<li>
+			<img src={imageUrl} alt={name} />
+			<div>
+				<p>{name}</p>
+				<p>{ingredients.join(', ')}</p>
+				<div>
+					{!soldOut ? (
+						<p>{formatCurrency(unitPrice)}</p>
+					) : (
+						<p>Sold out</p>
+					)}
+				</div>
+			</div>
+		</li>
+	);
 }
+
+MenuItem.propTypes = {
+	pizza: PropTypes.object.isRequired,
+	id: PropTypes.number,
+	name: PropTypes.string,
+	unitPrice: PropTypes.number,
+	ingredients: PropTypes.array,
+	soldOut: PropTypes.bool,
+	imageUrl: PropTypes.string,
+};
 
 export default MenuItem;
